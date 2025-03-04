4c6753955308d4dfd9874fd8c2417c84dd39af79
codeadhiraj <adhirajchakraborty492@gmail.com>
2025-03-04 06:55:14 +0000

    Your commit message here

---
+++ 01_basics/01_var.js
@@ -0,0 +1,23 @@
+
+var name = "John";
+console.log(name);
+
+
+let age = 25;
+console.log(age);
+
+
+const country = "India";
+console.log(country);
+
+
+age = 26;
+console.log("Updated Age:", age);
+
+
+let isStudent = true;        // Boolean
+let score = 98.5;           // Number
+let hobbies = ["Reading", "Gaming"]; // Array
+let person = { firstName: "John", lastName: "Doe" }; // Object
+
+console.log(isStudent, score, hobbies, person);
