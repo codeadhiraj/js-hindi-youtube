c8dd15d3655b341c6e279530ad6b2d3891271990
Adhiraj Chakraborty <adhirajchakraborty492@gmail.com>
2025-06-12 15:17:20 +0000

    done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+ "use strict";//treat all JS as newer version
+
+//  alert(3 + 3) // we are using nodejs not browser
+
+console.log("Adhiraj");
+
+let name="adhiraj"
+let age=19
+let isLoggedIn= false
+let state;
+
+// number=> 2 to the power 53
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol
+
+//  object
+
+console.log((typeof null));
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,15 @@
-console.log("Adhiraj")
+const accountId= 144553
+let accountEmail="adgoogle.com"
+var accountPassword="12345"
+accountCity="Siliguri"
+let accountState;
+
+// accountId=2
+
+accountEmail="aac@ac.com"
+accountPassword="21212121"
+accountCity="Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
